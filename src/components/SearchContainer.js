b42c0c57327220ab42d5800c83e6b5ee57a794ba
Maureen Mccallum <mccallummaureen@gmail.com>
2026-02-15 17:49:57 -0500

    Fix debounce stability in SearchContainer

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,8 +1,8 @@
-import { useState, useMemo } from 'react'
-import Wrapper from "../assets/wrappers/SearchContainer";
-import { FormRow, FormRowSelect } from "../components";
-import { useSelector, useDispatch } from "react-redux";
-import { handleChange, clearFilters } from "../features/allJobs/allJobsSlice";
+import Wrapper from '../assets/wrappers/SearchContainer';
+import { FormRow, FormRowSelect } from '.';
+import { useSelector, useDispatch } from 'react-redux';
+import { handleChange, clearFilters } from '../features/allJobs/allJobsSlice';
+import { useState, useMemo, useCallback } from 'react';
 
 const SearchContainer = () => {
   const [ localSearch, setLocalSearch ] = useState('')
@@ -10,78 +10,75 @@ const SearchContainer = () => {
     useSelector((store) => store.allJobs);
   const dispatch = useDispatch();
   const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
+  
   const handleSearch = (e) => {
-    // dispatch an action
     dispatch(handleChange({ name: e.target.name, value: e.target.value }));
   };
 
-  const debounce = () => {
-    let timeoutID;
+  // Use useCallback to create a stable debounce function
+  const debounce = useCallback(() => {
+    let timeoutId;
     return (e) => {
-      setLocalSearch(e.target.value)
-      clearTimeout(timeoutID)
-      timeoutID = setTimeout(() => {
-        dispatch(handleChange({ name: e.target.name, value: e.target.value}))
-      }, 1000)
-    }
-  }
+      setLocalSearch(e.target.value);
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+      }, 1000);
+    };
+  }, [dispatch]);
 
-  const optimizedDebounce = useMemo (() => debounce(), [debounce])
+  const optimizedDebounce = useMemo(() => debounce(), [debounce]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLocalSearch('')
-    // dispatch an action to clear filters
+    setLocalSearch('');
     dispatch(clearFilters());
   };
+
   return (
     <Wrapper>
-      <form className="form">
-        <h4>search form </h4>
-        <div className="form-center">
+      <form className='form'>
+        <h4>search form</h4>
+        <div className='form-center'>
           {/* search position */}
+
           <FormRow
-          type="text"
-          name="search"
-          value={localSearch}
-          handleChange={optimizedDebounce}
+            type='text'
+            name='search'
+            value={localSearch}
+            handleChange={optimizedDebounce}
           />
           {/* search by status */}
           <FormRowSelect
-            name="searchStatus"
-            labelText="status"
+            labelText='status'
+            name='searchStatus'
             value={searchStatus}
             handleChange={handleSearch}
             list={['all', ...statusOptions]}
           />
           {/* search by type */}
           <FormRowSelect
-            name="searchType"
-            labelText="Job Type"
+            labelText='type'
+            name='searchType'
             value={searchType}
             handleChange={handleSearch}
             list={['all', ...jobTypeOptions]}
-            
           />
           {/* sort */}
           <FormRowSelect
-            name="sort"
+            name='sort'
             value={sort}
             handleChange={handleSearch}
-            list={['all', ...sortOptions]}
-            />
-        </div>
-        <div style={{marginTop: "1rem", justifyContent: "center", alignItems: "center", display: "flex"}}>
-
-        <button
-          className="btn btn-danger"
-          style={{padding: "0.5rem 2rem", minWidth: "180px"}}
-          disabled={isLoading}
-          onClick={handleSubmit}
+            list={sortOptions}
+          />
+          <button
+            className='btn btn-block btn-danger'
+            disabled={isLoading}
+            onClick={handleSubmit}
           >
-          clear filters
-        </button>
-          </div>
+            clear filters
+          </button>
+        </div>
       </form>
     </Wrapper>
   );
